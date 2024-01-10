e7a82be648a5ea32b0992e7473df658ed331c507
tkmynrg <allamuradov@internetlab.ru>
2024-01-10 18:49:01 +0300

    refactor(extended-1.2): added media alts

--- app.js
+++ app.js
@@ -45,6 +45,7 @@ var createNewTaskElement=function(taskString){
 
   deleteButton.className="delete";
   deleteButtonImg.src='./remove.svg';
+  deleteButtonImg.alt = 'Icon remove';
   deleteButton.appendChild(deleteButtonImg);
 
 
